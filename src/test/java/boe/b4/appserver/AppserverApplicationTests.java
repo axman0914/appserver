6599e2d4ff6b4c9f6aec6474ccc237b8d6dd2119
axman0914 <312213159@qq.com>
2023-03-14 15:30:34 +0800

    Initial commit

---
+++ src/test/java/boe/b4/appserver/AppserverApplicationTests.java
@@ -0,0 +1,13 @@
+package boe.b4.appserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
